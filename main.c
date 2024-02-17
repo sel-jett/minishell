aea1b53afe4d8265a736258774757c1c1b2ba984
Amine El Hassak <amel-has@e3r4p5.1337.ma>
2024-02-17 18:53:08 +0100

    add mo

--- main.c
+++ main.c
@@ -35,36 +35,30 @@ int	main(void)
 {
 	t_list  *list;
 	int     i;
-
 	int		index;
 
 	(1== 1) && (list = NULL);
 	while (1)
 	{
-		(1 == 1) && (index = 0, i = 0, list = c_list(),list->str = NULL);
+		(1 == 1) && (index = 0, i = 0, list = c_list(), list->str = 0);
 		if (!list)
+			(1 == 1) && (printf("ERROR1\n"), index = 1);
+		if (!index)
 		{
-			printf("ERROR1\n");index = 1;
-			break;
-		}
-		list->str = readline("minishell > ");
-		if (!list->str)
-			index = 1;
-		add_history(list->str);
-		if (index == 0)
-		{
-			while (list->str[i])
+			list->str = readline("minishell > ");
+			if (!list->str)
+				index = 1;
+			if (!index)
+				add_history(list->str);
+			while (list->str && list->str[i])
 				if (!plants(list, &i))
 					break;
 			if (!plant_4(list))
-			{
-				printf("ERROR7\n");
-				index = 1;
-			}
+				(1 == 1) && (printf("ERROR7\n"), index = 1);
+			if (!index)
+				affiche(list);
+			my_malloc(0, 0);
 		}
-		if (index == 0)
-			affiche(list);
-		my_malloc(0, 0);
 	}
 	return (0);
 }
--- parcing/add_in_list/plant_1.c
+++ parcing/add_in_list/plant_1.c
@@ -6,56 +6,32 @@
 /*   By: amel-has <amel-has@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 03:00:33 by amel-has          #+#    #+#             */
-/*   Updated: 2024/02/17 18:08:39 by amel-has         ###   ########.fr       */
+/*   Updated: 2024/02/17 18:12:45 by amel-has         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static int	fun_1(t_list *list, int *i)
-{
-	
-	int		len;
-	char	*s;
-	t_node	*node;
-
-	(1 == 1) && (len = 1, *i += 1,node = 0);
-	while (list->str[*i + len] && list->str[*i + len] != '"')
-		len++;
-	if (list->str[*i + len] != '"')
-		return (0);
-	s = my_malloc (len * sizeof(char), 1);
-	if (!s)
-		return (0);
-	len = 0;
-	while (list->str[*i + len] && list->str[*i + len] != '"'){
-		s[len] = list->str[*i + len];len++;}
-	s[len] = '\0';
-	node = c_node(s, list->tail, TOKEN_Double_Q);
-	if (!node)
-		return (0);
-	add_back(list, node);
-	*i += len + 1;
-	return (1);
-}
-
-static int	fun_2(t_list *list, int *i)
+static int	fun_(t_list *list, int *i,char c)
 {
 	int		len;
 	char	*s;
 	t_node	*node;
 
 	(1 == 1) && (len = 1, *i += 1,node = 0);
-	while (list->str[*i + len] && list->str[*i + len] != '\'')
+	while (list->str[*i + len] && list->str[*i + len] != c)
 		len++;
-	if (list->str[*i + len] != '\'')
+	if (list->str[*i + len] != c)
 		return (0);
 	s = my_malloc (len * sizeof(char), 1);
 	if (!s)
 		return (0);
 	len = 0;
-	while (list->str[*i + len] && list->str[*i + len] != '\''){
-		s[len] = list->str[*i + len];len++;}
+	while (list->str[*i + len] && list->str[*i + len] != c)
+	{
+		s[len] = list->str[*i + len];
+		len++;
+	}
 	s[len] = '\0';
 	node = c_node(s, list->tail, TOKEN_Double_Q);
 	if (!node)
@@ -70,11 +46,11 @@ int	plant_1(t_list *list, int *i)
 	
 	if (list->str[*i] == '"')
 	{
-		if (!fun_1(list, i))
+		if (!fun_(list, i, '"'))
 			return (0);
 	}
 	else if (list->str[*i] == '\'')
-		if (!fun_2(list, i))
+		if (!fun_(list, i, '\''))
 			return (0);
 	return (1);
 }
