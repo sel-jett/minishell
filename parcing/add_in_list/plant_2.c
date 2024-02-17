b9e5e6e3c5a365830ad2acc008c061c28bf50cfa
Amine El Hassak <amel-has@e3r3p10.1337.ma>
2024-02-17 03:11:10 +0100

    add mo

--- parcing/add_in_list/plant_2.c
+++ parcing/add_in_list/plant_2.c
@@ -1,26 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   plant_2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amel-has <amel-has@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/17 03:04:14 by amel-has          #+#    #+#             */
+/*   Updated: 2024/02/17 03:06:27 by amel-has         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/minishell.h"
 
 int	check(int c)
 {
-	if (c && c != '|' && c != '&' && c !=
-		'<' && c != '>' && c != '\'' && c != '&' && c != '"' && c != ' ')
+	if (c && c != '|' && c != '&' && c != '<'
+		&& c != '>' && c != '\'' && c != '&' && c != '"' && c != ' ')
 		return (1);
 	return (0);
 }
 
-int		plant_2(char *str, t_list   *list, int *i)
+int	plant_2(char *str, t_list *list, int *i)
 {
-	int     len;
-	char    *s;
+	int		len;
+	char	*s;
 	t_node	*node;
 
-	len  = 0;
-	s = NULL;node = NULL;
+	(1 == 1) && (len = 0, node = 0, s = 0);
 	while (check(str[len]))
 		len++;
 	s = my_malloc(sizeof(char) * (len + 1), 1);
 	if (!s)
-		return(0);
+		return (0);
 	(1 == 1) && (*i += len, len = -1);
 	while (check(str[++len]))
 		s[len] = str[len];
--- parcing/add_in_list/plant_3.c
+++ parcing/add_in_list/plant_3.c
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   plant_3.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amel-has <amel-has@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/17 03:06:53 by amel-has          #+#    #+#             */
+/*   Updated: 2024/02/17 03:10:54 by amel-has         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/minishell.h"
 
-static  int	add_two(char *str, t_list   *list, int *i ,int mode)
+static int	add_two(char *str, t_list *list, int *i, int mode)
 {
 	char	*s;
 	int		j;
 	t_node	*node;
 
-	(1 == 1) && (j = -1, s = 0,node = 0);
+	(1 == 1) && (j = -1, s = 0, node = 0);
 	s = my_malloc(sizeof(char) * 3, 1);
 	if (!s)
 		return (0);
@@ -21,15 +33,15 @@ static  int	add_two(char *str, t_list   *list, int *i ,int mode)
 	return (1);
 }
 
-int		add_one(char *str, t_list   *list, int *i ,int mode)
+int	add_one(char *str, t_list *list, int *i, int mode)
 {
 	char	*s;
 	t_node	*node;
 
-	(1 == 1) && (s = 0,node = NULL);
+	(1 == 1) && (s = 0, node = 0);
 	s = my_malloc(sizeof(char) * 2, 1);
 	if (!s)
-		return(0);
+		return (0);
 	(1 == 1) && (s[0] = str[0], s[1] = '\0');
 	node = c_node(s, list->tail, mode);
 	if (!node)
@@ -39,9 +51,11 @@ int		add_one(char *str, t_list   *list, int *i ,int mode)
 	return (1);
 }
 
-int	plant_3(char *str, t_list   *list, int *i)
+int	plant_3(char *str, t_list *list, int *i)
 {
-	int index = 0;
+	int	index;
+
+	index = 0;
 	if (!str)
 		return (0);
 	if (str[0] == '<')
@@ -57,13 +71,15 @@ int	plant_3(char *str, t_list   *list, int *i)
 	}
 	else if (str[0] == '|')
 	{
-		if (str[1]){
-			if (str[1]  == '|')
+		if (str[1])
+		{
+			if (str[1] == '|')
 			{
 				if (!add_two(str, list, i, TOKEN_OR))
 					return (0);
 				index = 1;
-			}}
+			}
+		}
 		if (index == 0)
 			if (!add_one(str, list, i, TOKEN_PIPE))
 				return (0);
@@ -79,7 +95,7 @@ int	plant_3(char *str, t_list   *list, int *i)
 			if (!add_one(str, list, i, TOKEN_REDIR_OUT))
 				return (0);
 	}
-	else if(str[0] == '&')
+	else if (str[0] == '&')
 	{
 		if (str[1] && str[1] == '&')
 		{
