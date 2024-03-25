af99a25c8bed039b457485b0b940b0df18dbe3b4
sel-jett <sel-jett@e3r11p7.1337.ma>
2024-03-25 21:39:02 +0000

    one norms function

--- execution/utils_3.c
+++ execution/utils_3.c
@@ -6,7 +6,7 @@
 /*   By: sel-jett <sel-jett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 20:33:43 by sel-jett          #+#    #+#             */
-/*   Updated: 2024/03/25 19:54:27 by sel-jett         ###   ########.fr       */
+/*   Updated: 2024/03/25 21:37:37 by sel-jett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,18 @@ void	ft_execve(char *env_var, char **env, char **cmmd)
 	}
 }
 
-// char	*array_joner()
-// {}
+char	*arr_value(t_node *tmp, char **str, int *i)
+{
+	if (*str)
+		(1) && ((*i)++, str[*i] = ft_strjoin(str[*i], tmp->value), 0);
+	else
+		str[*i] = ft_strjoin(str[*i], tmp->value);
+	if (!str[*i])
+		return (NULL);
+	return (str[*i]);
+}
 
-char	**arr_filler(t_node	*tmp, char **arr, int i)
+char	**arr_filler(t_node	*tmp, char **a, int i)
 {
 	int		check;
 
@@ -41,27 +49,15 @@ char	**arr_filler(t_node	*tmp, char **arr, int i)
 			if (!tmp)
 				break ;
 			if (check == 0)
-			{
-				arr[i] = ft_strjoin(arr[i], "\\");
-				tmp->flag_space = 1;
-			}
+				(1) && (a[i] = ft_strjoin(a[i], "\\"), tmp->flag_space = 1);
 		}
 		if (tmp->value && !tmp->flag_space)
-		{
-			arr[i] = ft_strjoin(arr[i], tmp->value);
-			if (!arr[i])
-				return (NULL);
-		}
+			a[i] = ft_strjoin(a[i], tmp->value);
 		else if (tmp->value && tmp->flag_space == 1)
-		{
-			if (arr[i])
-				(1) && (i++, arr[i] = ft_strjoin(arr[i], tmp->value), 0);
-			else
-				arr[i] = ft_strjoin(arr[i], tmp->value);
-			if (!arr[i])
-				return (NULL);
-		}
+			a[i] = arr_value(tmp, a, &i);
+		if (!a[i])
+			return (NULL);
 		tmp = tmp->next;
 	}
-	return (i++, arr[i] = NULL, arr);
+	return (i++, a[i] = NULL, a);
 }
