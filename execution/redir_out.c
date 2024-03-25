59152e0c70b42a183a229f67b13302d16b9cf141
sel-jett <sel-jett@e3r11p7.1337.ma>
2024-03-25 15:54:01 +0000

    tsste

--- execution/redir_out.c
+++ execution/redir_out.c
@@ -6,7 +6,7 @@
 /*   By: sel-jett <sel-jett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 22:12:12 by sel-jett          #+#    #+#             */
-/*   Updated: 2024/03/25 15:12:23 by sel-jett         ###   ########.fr       */
+/*   Updated: 2024/03/25 15:52:37 by sel-jett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -141,6 +141,15 @@ int	files_opener(t_node_arbre *tree, t_files *file, t_env	*env)
 	return (1);
 }
 
+void	ft_close_all(t_files *file, int out_or, int in_or)
+{
+	(file->fd) && (ft_close_fd(file->fd), 0);
+	(file->sd) && (ft_close_fd(file->sd), 0);
+	(file->ad) && (ft_close_fd(file->ad), 0);
+	close(out_or);
+	close(in_or);
+}
+
 void	ft_execute_redir_out(t_node_arbre *tree, t_env	*env, t_env *exp)
 {
 	t_files			*file;
@@ -155,8 +164,7 @@ void	ft_execute_redir_out(t_node_arbre *tree, t_env	*env, t_env *exp)
 	if (!files_opener(tree, file, env))
 	{
 		ft_status(1, 1);
-		close(orig_stdout);
-		close(orig_stdin);
+		ft_close_all(file, orig_stdout, orig_stdin);
 		return ;
 	}
 	files_dupper(file->fd, file->sd, file->ad, cnt);
